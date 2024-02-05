3b960b5de2290355ebefd4d5533904768d22f855
Andre-John Mas <andrejohn.mas@gmail.com>
2024-02-05 16:48:55 -0500

    adjusting base path

--- quasar.config.js
+++ quasar.config.js
@@ -60,7 +60,7 @@ module.exports = configure(function (/* ctx */) {
         browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
         node: 'node20'
       },
-
+      appBase: '/looffi-treasure-hunter',
       vueRouterMode: 'hash', // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
